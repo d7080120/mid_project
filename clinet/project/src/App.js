7fd80ee6f1029bd49444d435cbb7213f87b3c24b
a48457 <38325366722@mby.co.il>
2024-12-22 19:21:42 +0200

    updat App.js

--- clinet/project/src/App.js
+++ clinet/project/src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbars/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/books' element={<Suspense fallback="loading..."><Books bookslist={bookslist}/></Suspense>} />
+        <Route path='/book/:id' element={<Suspense fallback="loading..."><Book bookslist={bookslist}/></Suspense> } />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
     </div>
   );
 }
